80bf107639d7d6db4be33285a318005819753b94
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 11:33:27 -0500

    This is an example test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('This is my first PROJECT!!!');
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
