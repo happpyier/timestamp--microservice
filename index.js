f8416274bfc476b8744000717b89e2c30f4eae2f
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 15:36:49 -0500

    heroku cool app using as a demo

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var http = require("http");
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
-}).listen(5000);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
