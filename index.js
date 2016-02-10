87df6dd1ff7e31504bc11b0657a37da572f2d145
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 11:36:34 -0500

    Another attempt at Hello World

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
-server.listen(8080);
+
+app.listen(80);
