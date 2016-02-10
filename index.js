5a8ab164663f156aa8c21d920945235952377c4c
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 16:43:51 -0500

    working on getting response and request to show up

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('The request is...'+JSON.parse(request)+'\n'+'The response is...'+JSON.parse(response));
+  response.send("Need to work on the Stringify part"+'\n'+'The request is...'+request+'\n'+'The response is...'+response);
   response.end('Its Over!');
 });
 
