ab60059bf4bf56b2513942d463ad4371088bc033
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 16:00:09 -0500

    testing the request parameter

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('This is a test!');
+  response.send('The request is...'+request);
 });
 
 app.listen(app.get('port'), function() {
