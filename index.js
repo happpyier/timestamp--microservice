0890e5e4ce1484d2875b24bc625aa9ea755a55b5
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 10:55:03 -0500

    Updated the test to work better???

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  res.send('This is my first PROJECT!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
