91b41c128481137dc9d145fb1b6f0053945cd528
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 11:06:47 -0500

    test failed, Third test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-/*
-app.set('port', (process.env.PORT || 5000));
-*/
+
+app.set('port', (process.env.PORT || 8080));
+
 app.get('/', function(request, response) {
   res.send('This is my first PROJECT!');
 });
