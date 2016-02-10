267040c9f757b87d631f17fa75d5cdc404cb32cd
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 11:16:36 -0500

    Found a sample online, trying it out

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 8080));
-
-app.get('/', function(request, response) {
-  res.send('This is my first PROJECT!');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
