f9ea819d753b27d0e0ba5331f13bd55b8b7e9c25
Thomas Bernfeld <happpyier@gmail.com>
2016-02-10 15:55:55 -0500

    heroku cool app using as a demo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  response.send('This is a test!');
 });
 
 app.listen(app.get('port'), function() {
