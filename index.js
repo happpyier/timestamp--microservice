f9996e8456235e7ef076327525d9b45c4c5e8159
Thomas Bernfeld <happpyier@gmail.com>
2016-02-25 09:04:01 -0500

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var reponseParsed = response; 
+  var reponseParsed = response._header; 
   response.send("Need to work on the Stringify part"+'\n'+'The request is...'+"Not Gonna Happen"+'\n'+'The response is...\n'+reponseParsed);
   response.end('Its Over!');
 });
