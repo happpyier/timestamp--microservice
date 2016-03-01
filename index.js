cf23e1627711d781e5cc18d1940e821059b36721
Thomas Bernfeld <happpyier@gmail.com>
2016-03-01 11:27:56 -0500

    trying stuff

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   var reponseParsed = JSON.stringify(response.connection.server.domain);
-  var reponseUnparsed = 'Object.keys(response.connection.server)';
+  var reponseUnparsed = Object.keys(response.connection.parser);
   response.send("Need to work on the Stringify part"+'\n'+'The reponse unParsed is...'+reponseUnparsed+'\n'+'The response is...\n'+reponseParsed);
   response.end('Its Over!');
 });
