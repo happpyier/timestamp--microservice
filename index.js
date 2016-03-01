c3accaa349bbfcdced9cb5e394bede4f48b7e6d0
Thomas Bernfeld <happpyier@gmail.com>
2016-03-01 11:14:32 -0500

    trying stuff

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-/*
+/* response
 domain,_events,_maxListeners,output,outputEncodings,outputCallbacks,writable,_last,chunkedEncoding,shouldKeepAlive,useChunkedEncodingByDefault,sendDate,_removedHeader,_hasBody,_trailer,finished,_hangupClose,_headerSent,socket,connection,_header,_headers,_headerNames,req,locals
+*/
+/* response.connection
+_connecting,_hadError,_handle,_parent,_host,_readableState,readable,domain,_events,_maxListeners,_writableState,writable,allowHalfOpen,destroyed,bytesRead,_bytesDispatched,_pendingData,_pendingEncoding,server,_idleTimeout,_idleNext,_idlePrev,_idleStart,parser,_paused,read,_consuming,_httpMessage 
 */
 var express = require('express');
 var app = express();
@@ -13,8 +16,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var reponseParsed = JSON.stringify(response._headerSent);
-  var reponseUnparsed = Object.keys(response.connection);
+  var reponseParsed = JSON.stringify(response.connection._host);
+  var reponseUnparsed = 'Object.keys(response.connection)';
   response.send("Need to work on the Stringify part"+'\n'+'The reponse unParsed is...'+reponseUnparsed+'\n'+'The response is...\n'+reponseParsed);
   response.end('Its Over!');
 });
