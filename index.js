f82d4afc9f0fe59f30b608f882bf981fb7888b17
Thomas Bernfeld <happpyier@gmail.com>
2016-03-01 10:14:05 -0500

    trying stuff

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   var reponseParsed = JSON.stringify(response._headerSent);
-  var reponseUnparsed = 'response.locals';
+  var reponseUnparsed = Object.keys(connection);
   response.send("Need to work on the Stringify part"+'\n'+'The reponse unParsed is...'+reponseUnparsed+'\n'+'The response is...\n'+reponseParsed);
   response.end('Its Over!');
 });
