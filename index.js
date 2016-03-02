e17f9cf61b0ef49e0a09557d19d3bffb22d02f65
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 14:21:10 -0500

    working out the dates

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/:tagId', function(request, response) {
   var reponseParsed = request.params.tagId;
-  //put a regular expression for data here!
+  //put a regular expression for data here! 1451624400
   var searchedString = /^\w*\s[1-9]+\S\s\d*$/.test(reponseParsed);
-  if (searchedString == true)
+  var searchedString2 = /^\d{8,}$/.test(reponseParsed);
+  if (searchString2  == true && reponseParsed > 86400000)
   {
-	response.send("{\"unix\":dateInUnixtime,\"natural\":\""+reponseParsed+"\"}");
+	var dateInNaturaltime = Date.parse("January 1, 2016")/ 1000;
+	response.send("{\"unix\":"+reponseParsed+",\"natural\":\""+dateInNaturaltime+"\"}");
+  }
+  else if (searchedString == true)
+  {
+	var dateInUnixtime = Date.parse(reponseParsed)/ 1000;
+	response.send("{\"unix\":"+dateInUnixtime+",\"natural\":\""+reponseParsed+"\"}");
   }
   else
   {
