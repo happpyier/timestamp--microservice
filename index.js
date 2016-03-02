002d868dd83cae65d5258e2d6056565f9f4f68d5
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 15:39:43 -0500

    working out the dates

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/:tagId', function(request, response) {
   var preReponseParsed = request.params.tagId;
-  ReponseParsed = preReponseParsed.replace(",", "");
+  var ReponseParsed = preReponseParsed.replace(",", "");
   var searchedString = /^\w*\s[1-9]+\S\s\d*$/.test(reponseParsed);
   var searchedString2 = /^\d{8,10}$/.test(reponseParsed);
-  /*
   if (searchedString2  == true)
   {
 	var d = new Date(ReponseParsed);
@@ -28,8 +27,7 @@ app.get('/:tagId', function(request, response) {
 		response.send("Please enter a date in the form of Unix Time(1451606400) or Natural Time(January 1, 2016)")
 	  }
   }
-  */
-  reponse.send("it got here");
+  //reponse.send("it got here");
 });
 
 app.get('/', function(request, response) {
