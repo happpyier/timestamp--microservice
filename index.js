62dbc2eebba4e61c67cec762d5640aec5eababa9
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 11:57:06 -0500

    working on the regular expression part

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/:tagId', function(request, response) {
   var reponseParsed = JSON.stringify(request.params.tagId);
   //put a regular expression for data here!
-  var searchedString = reponseParsed.search(/^\w\s\d\W\s\d$/gi);
+  var searchedString = /\w\s\d\S\s\d/.test(reponseParsed);
   response.send("You said<br/>"+searchedString);
   //response.end('Its Over!'); 
 });
