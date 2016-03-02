0e21e2b1c6a384bc5a57bbbaf88f342fb287e5bd
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 10:16:16 -0500

    Got the format...also updated the intro page

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/* ---request
-_readableState,readable,domain,_events,_maxListeners,socket,connection,httpVersionMajor,httpVersionMinor,httpVersion,complete,headers,rawHeaders,trailers,rawTrailers,_pendings,_pendingIndex,upgrade,url,method,statusCode,statusMessage,client,_consuming,_dumped,next,baseUrl,originalUrl,_parsedUrl,params,query,res,route
-*/
- 
 var express = require('express');
 var app = express();
 var path = require("path");
@@ -15,6 +11,7 @@ app.get('/:tagId', function(request, response) {
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
+  response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
