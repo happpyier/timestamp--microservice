d027196b9282f70741f45672f94e3968e01c9d57
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 15:01:34 -0500

    working out the dates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/:tagId', function(request, response) {
   var searchedString2 = /^\d{8,10}$/.test(reponseParsed);
   if (searchString2  == true && reponseParsed > 86400000)
   {
-	var dateInNaturaltime = Date.parse("January 1, 2016")/ 1000;
+	//var dateInNaturaltime = Date.parse("January 1, 2016")/ 1000;
 	/*response.send("{\"unix\":"+reponseParsed+",\"natural\":\""+dateInNaturaltime+"\"}"); */
 	response.send("it got here");
   }
