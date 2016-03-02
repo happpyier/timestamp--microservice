ec9c46823bd7f1339bb19562113b34dff9d9a7a4
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 12:21:51 -0500

    regular expression should work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/:tagId', function(request, response) {
   var reponseParsed = request.params.tagId;
   //put a regular expression for data here!
   var searchedString = /^\w*\s\d*\S\s\d*$/.test(reponseParsed);
-  response.send("You said<br/>"+searchedString);
+  response.send("{\"unix\":dateInUnixtime,\"natural\":\""+reponseParsed\"}");
   //response.end('Its Over!'); 
 });
 
