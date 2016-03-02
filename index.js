58dcdef04c6be1898261916dd82a944c6e12c3d4
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 15:42:30 -0500

    working out the kinks

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/:tagId', function(request, response) {
 	  }
   }
   */
-  reponse.send("it got here");
+  response.send("it got here");
 });
 
 app.get('/', function(request, response) {
