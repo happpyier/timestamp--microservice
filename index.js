98474992256837b33e2e971f2f2aa7e37c10db31
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 09:21:42 -0500

    requesting stuffs

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  var reponseParsed = JSON.stringify(request.baseUrl);
-  var reponseUnparsed = Object.keys(request.query); 
+  var reponseParsed = JSON.stringify(request.params.tagId);
+  var reponseUnparsed = Object.keys(request.params); 
   var hiiiKitty = request;
   if (hiiiKitty == "helloKitty")
   {
