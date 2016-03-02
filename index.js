b79720f659fbdb5c513027f72d24ac666c2f92a5
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 10:17:20 -0500

    Got the format...also updated the intro page

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/:tagId', function(request, response) {
   var reponseParsed = JSON.stringify(request.params.tagId);
   response.send("You said<br/>"+reponseParsed);
-  response.end('Its Over!'); 
+  //response.end('Its Over!'); 
 });
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
-  response.end('Its Over!'); 
+  //response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
