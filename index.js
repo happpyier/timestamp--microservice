f833c7e09e74a8c870119a6eb483cef274c2c87c
Thomas Bernfeld <happpyier@gmail.com>
2016-03-02 08:34:43 -0500

    trying stuffs

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-/* ---response
+/* ---request
 domain,_events,_maxListeners,output,outputEncodings,outputCallbacks,writable,_last,chunkedEncoding,shouldKeepAlive,useChunkedEncodingByDefault,sendDate,_removedHeader,_hasBody,_trailer,finished,_hangupClose,_headerSent,socket,connection,_header,_headers,_headerNames,req,locals
 */
-/* ---response.connection
-_connecting,_hadError,_handle,_parent,_host,_readableState,readable,domain,_events,_maxListeners,_writableState,writable,allowHalfOpen,destroyed,bytesRead,_bytesDispatched,_pendingData,_pendingEncoding,server,_idleTimeout,_idleNext,_idlePrev,_idleStart,parser,_paused,read,_consuming,_httpMessage 
-*/
-/* ---response.connection.server
-domain,_events,_maxListeners,_connections,_handle,_usingSlaves,_slaves,allowHalfOpen,pauseOnConnect,httpAllowHalfOpen,timeout,_connectionKey
-*/
-/* ---response.connection.parser
-0,1,2,3,_headers,_url,socket,incoming,maxHeaderPairs,onIncoming
-*/
-/* ---response.connection.parser.incoming
-_readableState,readable,domain,_events,_maxListeners,socket,connection,httpVersionMajor,httpVersionMinor,httpVersion,complete,headers,rawHeaders,trailers,rawTrailers,_pendings,_pendingIndex,upgrade,url,method,statusCode,statusMessage,client,_consuming,_dumped,next,baseUrl,originalUrl,_parsedUrl,params,query,res,_parsedOriginalUrl,route 
-*/  
+ 
 var express = require('express');
 var app = express();
 
@@ -20,7 +9,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   var reponseParsed = JSON.stringify(request.baseUrl);
-  var reponseUnparsed = 'id: ' + request.query.id; 
+  var reponseUnparsed = Object.keys(request); 
+  var reponseUnparsed111 = 'id: ' + request.query.id; 
   var hiiiKitty = request;
   if (hiiiKitty == "helloKitty")
   {
